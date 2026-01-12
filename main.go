47f7399cf2de6476bd42c399fd445cba1b217a62
Osathi Jantrasri <otfor01@gmail.com>
2026-01-12 11:42:13 +0700

    using env

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -14,11 +18,17 @@ func main() {
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	PORT := os.Getenv("PORT")
+
 	app.Get("/book", getBooks)
 	app.Get("/book/:id", getBook)
 	app.Post("/book", createBook)
 	app.Put("/book/:id", updateBook)
 	app.Delete("/book/:id", deleteBook)
 
-	app.Listen(":8080")
+	app.Listen(":" + PORT)
 }
