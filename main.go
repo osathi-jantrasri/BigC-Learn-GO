4f2287e1232b79dfbb05bfe7ddd3eb7b8d529a07
Osathi Jantrasri <otfor01@gmail.com>
2026-01-12 14:31:19 +0700

    middleware logger

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"fmt"
+  	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -18,11 +20,7 @@ func main() {
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	PORT := os.Getenv("PORT")
+	app.Use(loggingMiddleware)
 
 	app.Get("/book", getBooks)
 	app.Get("/book/:id", getBook)
@@ -30,5 +28,18 @@ func main() {
 	app.Put("/book/:id", updateBook)
 	app.Delete("/book/:id", deleteBook)
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	PORT := os.Getenv("PORT")
 	app.Listen(":" + PORT)
+}
+
+func loggingMiddleware(c *fiber.Ctx) error {
+	start := time.Now()
+	err := c.Next()
+	duration := time.Since(start)
+	fmt.Printf("[%s] %s %s %v\n", start.Format(time.RFC3339), c.Method(), c.OriginalURL(), duration)
+	return err
 }
